55a9fed80b83ce48906bd4634283e55a60a4b215
Ricky Chien <ricky060709@gmail.com>
2015-08-22 12:56:16 +0800

    add help-me-take

--- src/js/app-router.js
+++ src/js/app-router.js
@@ -3,22 +3,16 @@ import Router from 'react-router';
 import AppRouter from './app-router';
 
 import App from './components/app';
-import Dashboard from './components/dashboard';
-import Store from './components/store';
-import Preference from './components/preference';
-import Login from './components/login';
 import Home from './components/home';
+import HelpMeTake from './components/help-me-take';
 
 let Route = Router.Route;
 let DefaultRoute = Router.DefaultRoute;
 
 export default (
   <Route name='root' path='/' handler={App}>
-    <DefaultRoute handler={Dashboard}/>
-    <Route name='dashboard' handler={Dashboard} />
-    <Route name='store' handler={Store} />
-    <Route name='preference' handler={Preference} />
-    <Route name='login' handler={Login} />
+    <DefaultRoute handler={Home}/>
     <Route name='home' handler={Home} />
+    <Route name='help-me-take' handler={HelpMeTake} />
   </Route>
 );
